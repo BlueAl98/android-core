ef214244333a78ffad8c1ea5488dddefe2c67296
najib <alejandrorod1498@gmai.com>
2026-06-05 09:41:56 -0600

    fix datastore module inclusion

--- datastore/build.gradle.kts
+++ datastore/build.gradle.kts
@@ -47,7 +47,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.BlueAl98"
                 artifactId = "datastore"
-                version = "1.0.4"
+                version = "1.0.5"
             }
         }
     }
