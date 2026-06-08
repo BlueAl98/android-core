4c605c51161fb2463b9ce8bf306a0978e3b6a7bc
najib <alejandrorod1498@gmai.com>
2026-06-08 13:39:31 -0600

    datastore configure vanniktech maven publish plugin

--- datastore/build.gradle.kts
+++ datastore/build.gradle.kts
@@ -1,23 +1,20 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.29.0"
     id("signing")
 }
 
-
-group = "io.github.BlueAl98"
-version = "1.0.0"
 android {
-
     namespace = "com.nayibit.datastore"
     compileSdk = 35
 
     defaultConfig {
         minSdk = 29
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -38,25 +35,12 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
-    }
-
-
-
 }
 
 dependencies {
-
     ksp(libs.hilt.android.compiler)
     implementation(libs.hilt.android)
-
-    implementation (libs.androidx.datastore.preferences) // Asegúrate de usar la versión adecuada
-
-
+    implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -65,48 +49,39 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 }
 
-
 signing {
     useGpgCmd()
-    sign(publishing.publications)
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                from(components["release"])
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 
-                groupId = "io.github.BlueAl98"
-                artifactId = "datastore"
-                version = "1.0.0"
+    coordinates("io.github.blueal98", "datastore", "1.0.0")
 
-                pom {
-                    name.set("Nayibit Datastore")
-                    description.set("Android DataStore library")
-                    url.set("https://github.com/BlueAl98/android-core")
+    pom {
+        name.set("Nayibit Datastore")
+        description.set("Android DataStore library")
+        url.set("https://github.com/BlueAl98/android-core")
 
-                    licenses {
-                        license {
-                            name.set("Apache License 2.0")
-                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.set("BlueAl98")
-                            name.set("Najib Loera")
-                        }
-                    }
-
-                    scm {
-                        connection.set("scm:git:git://github.com/BlueAl98/android-core.git")
-                        developerConnection.set("scm:git:ssh://github.com/BlueAl98/android-core.git")
-                        url.set("https://github.com/BlueAl98/android-core")
-                    }
-                }
+        licenses {
+            license {
+                name.set("Apache License 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
             }
+        }
+
+        developers {
+            developer {
+                id.set("BlueAl98")
+                name.set("Najib Loera")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/BlueAl98/android-core.git")
+            developerConnection.set("scm:git:ssh://github.com/BlueAl98/android-core.git")
+            url.set("https://github.com/BlueAl98/android-core")
         }
     }
 }
