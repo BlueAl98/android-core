6fe105146bc431d21e1d4a163e562ea79e48fe5c
BlueAl98 <alejandrorod1498@gmail.com>
2026-07-21 12:34:07 -0600

    update library composables

--- composables/build.gradle.kts
+++ composables/build.gradle.kts
@@ -59,7 +59,7 @@ mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
 
-    coordinates("io.github.blueal98", "composables", "1.0.3")
+    coordinates("io.github.blueal98", "composables", "1.0.4")
 
     pom {
         name.set("Nayibit Composables")
